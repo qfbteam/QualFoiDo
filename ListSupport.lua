3b115a963bb8e9a9c28c0c85df2e4b507171a157
qfbteam <durkmcteam@gmail.com>
2024-09-28 19:43:14 -0400

    Update ListSupport.lua

--- ListSupport.lua
+++ ListSupport.lua
@@ -1,4 +1,18 @@
 local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/ui.lua'))()
+ArrayField:Notify({
+   Title = "Aviso!",
+   Content = "Pegue a keysystem do nosso script para fortalecer nossa equipe e continuar com o trabalho!",
+   Duration = 12,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+         print("The user tapped Okay!")
+      end
+   },
+ },
+})
 local Window = ArrayField:CreateWindow({
     Name = "QFB|Lista de jogos",
     LoadingTitle = "Carregando...",
@@ -13,23 +27,36 @@ local Window = ArrayField:CreateWindow({
        Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = true, -- Set this to true to use our key system
+    KeySystem = false, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Untitled",
+       Title = "QFB",
        Subtitle = "Key System",
-       Note = "O primeiro script hub/script br",
+       Note = "Versão: 2.0",
        FileName = "Key", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
        Actions = {
              [1] = {
                  Text = 'Clique aqui para copiar a key <--',
-                 OnPress = function()
+                 OnPress = function()               setclipboard("gateway.platoboost.com/l/24151/Durkmcteaming")            ArrayField:Notify({
+   Title = "Copiado!",
+   Content = "Link: gateway.platoboost.com/l/24151/Durkmcteaming",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+         print("The user tapped Okay!")
+      end
+   },
+ },
+})
                      print('Pressed')
                  end,
                  }
              },
-       Key = {"durk.hts"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Key = {"QFBKEY_D1L4I9"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
  local Tab = Window:CreateTab("Game List", 4483362458) -- Title, Image
@@ -47,6 +74,22 @@ local Button = Tab:CreateButton({
    Name = "Prision Life V0.8",
    Interact = 'Click',
    Callback = function()         loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/prision.lua"))()
+   -- The function that takes place when the button is pressed
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Doors V0.2",
+   Interact = 'Click',
+   Callback = function()               loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/doors.lua"))()
+   -- The function that takes place when the button is pressed
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Blade Ball V0.1",
+   Interact = 'Click',
+   Callback = function()               loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/bladeball.lua"))()
    -- The function that takes place when the button is pressed
    end,
 })
